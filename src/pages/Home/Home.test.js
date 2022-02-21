f4e3dcf7f217949903ffb008e4a172b4e2438a8b
BonhaengLee <bon952014@ajou.ac.kr>
2022-02-22 03:57:47 +0900

    screen.getByText()

--- src/pages/Home/Home.test.js
+++ src/pages/Home/Home.test.js
@@ -5,7 +5,7 @@ import Home from "./";
 describe("Home", () => {
   it("renders Home component", () => {
     render(<Home />);
-    // 있다는 것을 확신하기 위해서
-    screen.debug();
+
+    screen.getByText("Search:");
   });
 });
