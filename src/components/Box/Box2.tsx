a7fe694da571f27ddf249865f382333b016971d2
BonhaengLee <bon952014@ajou.ac.kr>
2022-06-18 23:36:45 +0900

    Events & drag box

--- src/components/Box/Box2.tsx
+++ src/components/Box/Box2.tsx
@@ -1,5 +1,26 @@
+import { motion } from "framer-motion";
+
 const Box2 = (): React.ReactElement => {
-  return <></>;
+  return (
+    <div className="box-container">
+      <motion.div
+        className="box"
+        whileHover={{
+          scale: 1.1,
+        }}
+        whileTap={{
+          scale: 0.9,
+        }}
+        drag
+        dragConstraints={{
+          right: 20,
+          left: -20,
+          top: 5,
+          bottom: 5,
+        }}
+      ></motion.div>
+    </div>
+  );
 };
 
 export default Box2;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,12 @@
-import Box1 from "../../components/Box/Box1";
+// import Box1 from "../../components/Box/Box1";
+import Box2 from "../../components/Box/Box2";
 import "../../styles/Home.module.scss";
 
 export default function App() {
   return (
     <div className="App">
-      <Box1 />
+      {/* <Box1 /> */}
+      <Box2 />
     </div>
   );
 }
