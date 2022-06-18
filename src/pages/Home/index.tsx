d76145590077c93040311153b9bc741cfe667327
BonhaengLee <bon952014@ajou.ac.kr>
2022-06-19 00:24:15 +0900

    Varianst 예제(when, staggerChildren,,,)

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,10 @@
-// import Box1 from "../../components/Box/Box1";
-import Box2 from "../../components/Box/Box2";
+import Box3 from "../../components/Box/Box3";
 import "../../styles/Home.module.scss";
 
 export default function App() {
   return (
     <div className="App">
-      {/* <Box1 /> */}
-      <Box2 />
+      <Box3 />
     </div>
   );
 }
