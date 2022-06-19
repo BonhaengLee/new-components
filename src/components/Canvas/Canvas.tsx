f8b50dfc90d2ead26bc2d1f2b44c9cb621a1ba51
BonhaengLee <bon952014@ajou.ac.kr>
2022-06-19 17:29:33 +0900

    Flower rain effect++

--- src/components/Canvas/Canvas.tsx
+++ src/components/Canvas/Canvas.tsx
@@ -1,53 +1,25 @@
-import classNames from "classnames/bind";
 import styles from "./Canvas.module.scss";
-
-const cx = classNames.bind(styles);
 
 const Canvas = (): React.ReactElement => {
   return (
     <div className={styles.canvas}>
-      <div className={styles.flower} id={styles.flower1}>
-        <div className={cx(styles.petal, styles.petal1, styles.f1p)}></div>
-        <div className={cx(styles.petal, styles.petal2, styles.f1p)}></div>
-        <div className={cx(styles.petal, styles.petal3, styles.f1p)}></div>
-        <div className={cx(styles.petal, styles.petal4, styles.f1p)}></div>
-        <div className={cx(styles.petal, styles.petal5, styles.f1p)}></div>
-      </div>
-      <div className={styles.flower} id={styles.flower2}>
-        <div className={cx(styles.petal, styles.petal1, styles.f2p)}></div>
-        <div className={cx(styles.petal, styles.petal2, styles.f2p)}></div>
-        <div className={cx(styles.petal, styles.petal3, styles.f2p)}></div>
-        <div className={cx(styles.petal, styles.petal4, styles.f2p)}></div>
-        <div className={cx(styles.petal, styles.petal5, styles.f2p)}></div>
-      </div>
-      <div className={styles.flower} id={styles.flower3}>
-        <div className={cx(styles.petal, styles.petal1, styles.f3p)}></div>
-        <div className={cx(styles.petal, styles.petal2, styles.f3p)}></div>
-        <div className={cx(styles.petal, styles.petal3, styles.f3p)}></div>
-        <div className={cx(styles.petal, styles.petal4, styles.f3p)}></div>
-        <div className={cx(styles.petal, styles.petal5, styles.f3p)}></div>
-      </div>
-      <div className={styles.flower} id={styles.flower4}>
-        <div className={cx(styles.petal, styles.petal1, styles.f4p)}></div>
-        <div className={cx(styles.petal, styles.petal2, styles.f4p)}></div>
-        <div className={cx(styles.petal, styles.petal3, styles.f4p)}></div>
-        <div className={cx(styles.petal, styles.petal4, styles.f4p)}></div>
-        <div className={cx(styles.petal, styles.petal5, styles.f4p)}></div>
-      </div>
-      <div className={styles.flower} id={styles.flower5}>
-        <div className={cx(styles.petal, styles.petal1, styles.f5p)}></div>
-        <div className={cx(styles.petal, styles.petal2, styles.f5p)}></div>
-        <div className={cx(styles.petal, styles.petal3, styles.f5p)}></div>
-        <div className={cx(styles.petal, styles.petal4, styles.f5p)}></div>
-        <div className={cx(styles.petal, styles.petal5, styles.f5p)}></div>
-      </div>
-      <div className={styles.flower} id={styles.flower6}>
-        <div className={cx(styles.petal, styles.petal1, styles.f6p)}></div>
-        <div className={cx(styles.petal, styles.petal2, styles.f6p)}></div>
-        <div className={cx(styles.petal, styles.petal3, styles.f6p)}></div>
-        <div className={cx(styles.petal, styles.petal4, styles.f6p)}></div>
-        <div className={cx(styles.petal, styles.petal5, styles.f6p)}></div>
-      </div>
+      <div className={styles.flowerA} id={styles.flower1}></div>
+      <div className={styles.flowerC} id={styles.flower2}></div>
+      <div className={styles.flowerB} id={styles.flower3}></div>
+      <div className={styles.flowerC} id={styles.flower4}></div>
+      <div className={styles.flowerA} id={styles.flower5}></div>
+      <div className={styles.flowerB} id={styles.flower6}></div>
+      <div className={styles.flowerA} id={styles.flower7}></div>
+      <div className={styles.flowerC} id={styles.flower8}></div>
+      <div className={styles.flowerB} id={styles.flower9}></div>
+      <div className={styles.flowerA} id={styles.flower10}></div>
+      <div className={styles.flowerC} id={styles.flower11}></div>
+      <div className={styles.flowerB} id={styles.flower12}></div>
+      <div className={styles.flowerA} id={styles.flower13}></div>
+      <div className={styles.flowerC} id={styles.flower14}></div>
+      <div className={styles.flowerB} id={styles.flower15}></div>
+      <div className={styles.flowerA} id={styles.flower16}></div>
+      <div className={styles.flowerC} id={styles.flower17}></div>
     </div>
   );
 };
